2b8a8c182dcd64cf382279bc2f528b95dab8add0
shawn <shawnbuttrick@gmail.com>
2019-04-15 02:41:13 +0000

    adding TODO notes for tests

--- test_mbta.py
+++ test_mbta.py
@@ -1,15 +1,41 @@
 import pytest
 import mock
 
-def test_junk():
-    pass
+def test_main():
+    assert False, 'todo'
 
-def test
-def fake():
-def main():
-def get_routes():
-def get_routes_to_stops(route_data):
-def show_min_and_max_stops(routeIds_to_stopIds):
-def build_route_intersections(set_of_stops, routeIds_to_stopIds):
-def show_intersections(stopIds_to_routeIds, routeId_to_routeLongName):
-def directions(r_to_s, s_to_r):
+def test_get_routes():
+    '''
+    todo:  mock call to MBTA ROUTES API
+    '''
+    assert False, 'todo'
+
+def test_get_routes_to_stops(route_data):
+    '''
+    todo:  mock call to MBTA STOPS API
+    '''
+    assert False, 'todo'
+
+def test_show_min_and_max_stops(routeIds_to_stopIds):
+    '''
+    todo: pass in some fabricated data and assert on outputs 
+    '''
+    assert False, 'todo'
+
+def test_build_route_intersections(set_of_stops, routeIds_to_stopIds):
+    '''
+    todo: pass in some fabricated data and assert on outputs 
+    '''
+    assert False, 'todo'
+
+def test_show_intersections(stopIds_to_routeIds, routeId_to_routeLongName):
+    '''
+    todo: pass in some fabricated data and assert on outputs 
+    '''
+    assert False, 'todo'
+
+def test_directions(r_to_s, s_to_r):
+    '''
+    todo:  include multiple test scenarios  
+    '''
+    assert False, 'todo'
